165513baa8d5871af36f3dbdf3abee49089a1cc6
Zaklbird <rami.garah@outlook.com>
2025-07-12 01:47:21 +0100

    Update: lets get started

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Tailwind is working! 🎉</h1>
-    </main>
+      <Link href={'/login'}>
+        <h1 className="text-4xl font-bold text-blue-600" >
+          lets get started
+        </h1>
+      </Link>
+    </main >
   );
 }
